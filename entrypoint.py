ea618a21cda6ebafd90da09303c748eb3423124c
fallbackerik <fallbackerik@gmail.com>
2024-06-10 18:02:35 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -4,7 +4,17 @@ import glob
 
 import xml.etree.ElementTree as etree
 
+SVG_NAMESPACE = "https://www.w3.org/2000/svg"
+
 for file in glob.glob(sys.argv[2]):
   print("opening...", file)
   tree = etree.parse(file)
   root = tree.getroot()
+
+  if not root.tag.startswith(f"{{{SVG_NAMESPACE}}}"):
+    root.attrib["xmlns"] = SVG_NAMESPACE
+    tree = etree.ElementTree(root)
+
+  if "width" not in root.attrib or "height" not in root.attrib:
+    raise ValueError("Expected the svg root element to contain width and height attributes.")
+  print("attrib", root.attrib)
