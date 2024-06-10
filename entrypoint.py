30ec94c5fdab51fcac437f486c5f4088cbe5bc37
fallbackerik <fallbackerik@gmail.com>
2024-06-10 11:55:53 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -2,5 +2,5 @@ import sys
 import os
 import datetime
 
-os.environ["GITHUB_OUTPUT"] = f"time={datetime.datetime.now())}"
+os.environ["GITHUB_OUTPUT"] = f"time={datetime.datetime.now()}"
 print("Hello ", sys.argv[1], "!")
