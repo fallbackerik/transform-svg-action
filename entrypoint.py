fa7e8e9e8e3e42c73179d2edc66ae22f2ef0cee2
fallbackerik <fallbackerik@gmail.com>
2024-06-10 17:07:42 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,10 @@
 import sys
 import os
 import datetime
+import glob
+
+print(glob.glob(sys.argv[1]))
+
 with open(os.environ["GITHUB_OUTPUT"], "a") as output:
   output.write(f"time={datetime.datetime.now()}\n")
   output.write(f"svgs-modified=1.svg 2.svg 3.svg\n")
