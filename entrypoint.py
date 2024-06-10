f5efc2225600ec05d1da80251d8bfc5e7b336070
fallbackerik <fallbackerik@gmail.com>
2024-06-10 17:56:17 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -6,5 +6,5 @@ import xml.etree.ElementTree as etree
 
 for file in glob.glob(sys.argv[2]):
   print("opening...", file)
-  with etree.parse(file) as tree:
-    pass
+  tree = etree.parse(file)
+  root = tree.getroot()
