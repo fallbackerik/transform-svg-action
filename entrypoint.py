3ea154f56acd515f69825083a6e6d8641c158b9c
fallbackerik <fallbackerik@gmail.com>
2024-06-10 16:20:08 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,5 +3,5 @@ import os
 import datetime
 
 os.environ["GITHUB_OUTPUT"] = f"time={datetime.datetime.now()}"
-os.environ["GITHUB_OUTPUT"] = f" svgs-modified=1.svg 2.svg 3.svg"
-print("Hello ", str(sys.argv), "!")
+os.environ["GITHUB_OUTPUT"] += f" svgs-modified=1.svg 2.svg 3.svg"
+print("Hello ", str(sys.argv), ":", os.environ["GITHUB_OUTPUT"], "!")
