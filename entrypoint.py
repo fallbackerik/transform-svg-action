e94cd4704ff35b032d82e6142249f8256a264209
fallbackerik <fallbackerik@gmail.com>
2024-06-10 16:23:12 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import datetime
-
+print("before", os.environ["GITHUB_OUTPUT"])
 os.environ["GITHUB_OUTPUT"] = f"time={datetime.datetime.now()}"
 os.environ["GITHUB_OUTPUT"] += f" svgs-modified=1.svg 2.svg 3.svg"
 print("Hello ", str(sys.argv), ":", os.environ["GITHUB_OUTPUT"], "!")
